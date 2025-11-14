fb286fefe3d5e2df24dcf4bf25488ddce677bb01
Matevzz1 <matevz.zvir.20660@dijak.sc-celje.si>
2025-11-14 09:21:31 +0100

    dodal konstruktorje

--- Vaja07SKUPNA/Pit_Trikotnik.cs
+++ Vaja07SKUPNA/Pit_Trikotnik.cs
@@ -11,5 +11,17 @@ namespace Vaja07SKUPNA
         private double a;
         private double b;
         private double c;
+        public Pit_Trikotnik()
+        {
+            a = 3;
+            b = 4;
+            c = 5;
+        }
+        public Pit_Trikotnik(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+            c = Math.Sqrt(a * a + b * b);
+        }
     }
 }
