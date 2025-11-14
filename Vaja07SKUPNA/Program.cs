fd59460edc952fd071f0b96f9b82d5cfe7e6317c
Matevzz1 <matevz.zvir.20660@dijak.sc-celje.si>
2025-11-14 09:30:29 +0100

    dodal v main

--- Vaja07SKUPNA/Program.cs
+++ Vaja07SKUPNA/Program.cs
@@ -10,6 +10,10 @@ namespace Vaja07SKUPNA
     {
         static void Main(string[] args)
         {
+            double a = 6;
+            double b = 8;
+            Pit_Trikotnik trikotnik = new Pit_Trikotnik(a, b);
+            Console.WriteLine("Ploščina pravokotnega trikotnika s katetama {0} in {1} je {2}.", a, b, trikotnik.IzracunajPloscino());
         }
     }
 }
